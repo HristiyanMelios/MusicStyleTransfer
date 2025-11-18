95187097701a5cac3d582a1791bd3eb95eaded3a
HristiyanMelios <124943450+HristiyanMelios@users.noreply.github.com>
2025-11-18 15:21:01 -0800

    Skeleton for compute_mels

--- lib/compute_mels.py
+++ lib/compute_mels.py
@@ -2,3 +2,16 @@
 Small script to precompute Mel spectrograms from the FMA small dataset,
 so we do not have to compute them again.
 """
+
+import os
+from audio.py import load_audio, audio_to_mel
+
+# Load dataset here using os
+
+def compute_mels(dataset_path, args):
+    # For loop here going through entire dataset
+    y, sr = load_audio(dataset_path, args)
+
+    # S = audio_to_mel(y, args)
+    # os.path.join(datasetpath + name of the file)
+    # numpy save or librosa save here using filepath
